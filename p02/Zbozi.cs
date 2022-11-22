0027c390ed9c16988f9a137495d5aa2acfbc1415
Ridanss <pavlonk.04@spst.eu>
2022-11-22 11:33:27 +0100

    p02

---
+++ p02/Zbozi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p02
+{
+    internal class Zbozi
+    {
+        private string nazev;
+        private int cena;
+        private DateTime datumVyroby;
+        private int dniTrvanlivosti;
+
+        public int MinTrvanlivostDny()
+        {
+            
+        }
+
+        public int NovaCena()
+        {
+
+        }
+    }
+}
