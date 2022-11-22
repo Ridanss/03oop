dbacb0b8311deb2dafd902cbc784209355be721d
Ridanss <pavlonk.04@spst.eu>
2022-11-22 11:23:38 +0100

    Přidejte soubory projektu.

---
+++ p01/Kostka.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p01
+{
+    internal class Kostka
+    {
+        /// <summary>
+        /// generátor náhodných čísel
+        /// </summary>
+        private Random random;
+        /// <summary>
+        /// počet stěn kostky
+        /// </summary>
+        private int pocetSten;
+
+        public Kostka()
+        {
+            pocetSten = 6;
+            random = new Random();
+        }
+
+        public Kostka(int pocetSten)
+        {
+            this.pocetSten = pocetSten;
+            random = new Random();
+        }
+
+        public int VratPocetSten()
+        {
+            return pocetSten;
+        }
+
+        public override string ToString()
+        {
+            return "kostka s " + pocetSten + " stěnami";
+        }
+
+        public int Hod()
+        {
+            return random.Next(1, pocetSten + 1);
+        }
+    }
+}
---
+++ p01/Zdravice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p01
+{
+    internal class Zdravice
+    {
+
+        public string text;
+
+        /// <summary>
+        /// Pozdraví uživatele textem pozdravu a jeho jménem
+        /// </summary>
+        /// <param name="jmeno">jméno uživatele</param>
+        /// <returns></returns> 
+        public string Pozdrav(string jmeno)
+        {
+            return String.Format($"{text} {jmeno}");    
+
+        }
+
+        
+    }
+}
