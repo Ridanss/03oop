abaa2ab4378200366992976965fceed2ae275172
Ridanss <pavlonk.04@spst.eu>
2022-12-06 11:22:14 +0100

    p04

---
+++ p04/Clovek.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p04
+{
+    internal class Clovek
+    {
+
+        private string jmeno;
+
+        private DateTime narozen;
+
+        public Clovek(string jmeno, DateTime narozen)
+        {
+            this.jmeno = jmeno;
+            this.narozen = narozen;
+        }
+
+        public void NastavJmeno(string noveJmeno)
+        {
+            jmeno = noveJmeno;
+        }
+
+        public string VypisJmeno()
+        {
+            return jmeno;
+        }
+
+        public int Vek()
+        {
+            int vek;
+            DateTime today = DateTime.Today;
+            vek = today.Year - narozen.Year;
+
+            if (today.DayOfYear < narozen.DayOfYear) vek--;
+            return vek;
+        }
+
+        public bool Plnolety()
+        {
+            if (Vek() >= 18) return true;
+            return false;
+        }
+
+        public string Starsi(Clovek druhyClovek)
+        {
+            if (druhyClovek.narozen < narozen)
+            {
+                return druhyClovek.jmeno + " je starší než " + jmeno + ".";
+            }
+            else return jmeno + " je starší než " + druhyClovek.jmeno + ".";
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{jmeno} má {Vek()} let a {(Plnolety() ? "je" : "není")} plnoletý");
+        }
+
+
+    }
+}
---
+++ p04/Form4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p04
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string jmeno1 = Convert.ToString(textBox1.Text);
+            string jmeno2 = Convert.ToString(textBox3.Text);
+            DateTime narozen1 = Convert.ToDateTime(textBox2.Text);
+            DateTime narozen2 = Convert.ToDateTime(textBox4.Text);
+
+            Clovek pepa = new Clovek(jmeno1, narozen1);
+            Clovek karel = new Clovek(jmeno2, narozen2);
+
+            MessageBox.Show(pepa.ToString());
+            pepa.NastavJmeno("Pepíno");
+            MessageBox.Show(pepa.ToString());
+
+            MessageBox.Show(karel.Starsi(pepa));
+        }
+    }
+}
