16d240c73960fa852ba6f541e7b07690584456b7
Ridanss <pavlonk.04@spst.eu>
2022-12-06 11:32:38 +0100

    p05

---
+++ p05/Retez.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p05
+{
+    internal class Retez
+    {
+
+        private string veta;
+
+        private int cislo;
+
+        public Retez(string veta)
+        {
+            this.veta = veta;
+            cislo = 1;
+        }
+
+        public Retez(string veta, int cislo)
+        {
+            this.veta = veta;
+            this.cislo = cislo;
+        }
+
+        public override string ToString()
+        {
+            return veta;
+        }
+
+        public void Zkrat()
+        {
+            bool slovo = true;
+            int pocetSlov = 0;
+            string chain = string.Empty;
+
+            for (int i = 0; i < veta.Length; i++)
+            {
+
+                if (char.IsLetter(veta[i]) || char.IsDigit(veta[i]))
+                {
+                    if (slovo)
+                    {
+                        pocetSlov++;
+                        if (pocetSlov <= cislo)
+                        {
+                            
+                        }
+                        slovo = false;
+                    }
+                }
+
+                else if (veta[i] == ' ')
+                {
+                    slovo = true;
+                }
+
+            }
+            
+        }
+
+    }
+}
