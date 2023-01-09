e989a1018148aebdd87096bf20c7a2cf13fc6da4
Ridanss <pavlonk.04@spst.eu>
2023-01-09 09:35:24 +0100

    p06

--- p06/Student.cs
+++ p06/Student.cs
@@ -8,6 +8,25 @@ namespace p06
 {
     internal class Student
     {
+        public string Jmeno { get; }
 
+        public string Prijmeni { get; set; }
+
+        private DateTime narozeni;
+        public DateTime DatumNarozeni
+        {
+            get
+            {
+                return narozeni;
+            }
+            set
+            {
+                narozeni = value;
+                if (narozeni > DateTime.Today)
+                {
+
+                }
+            }
+        }
     }
 }
