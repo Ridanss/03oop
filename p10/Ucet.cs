1197489e04c17a843d035ed8c3b0e874c36361fc
Ridanss <pavlonk.04@spst.eu>
2023-01-23 09:31:36 +0100

    p10

---
+++ p10/Ucet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p10
+{
+    internal class Ucet
+    {
+
+        public static int cisloUctu;
+
+        public string jmeno { get; set; }
+
+        public int castka;
+
+        public enum BankaEnum
+        {
+            Aktivni,
+            Zablokovany,
+            Zruseny
+        }
+
+        public Ucet(string jmeno, int castka)
+        {
+            this.jmeno = jmeno;
+
+            this.castka = 1000;
+        }
+    }
+}
